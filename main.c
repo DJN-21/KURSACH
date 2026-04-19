3547c2d11c834a8c1fa6a9364f0ee6cd0317a240
DJN-21 <evgenijblinov714@gmail.com>
2026-04-19 16:06:36 +0300

    Update main.c

--- main.c
+++ main.c
@@ -2,18 +2,16 @@
 #include <locale.h>
 #include "menu.h"
 #include "functions.h"
-
 int main() {
 	SetConsoleCP(65001);
 	SetConsoleOutputCP(65001);
 	setlocale(LC_ALL, ".UTF8");
-
 	struct menu_item menu_items[4] =
 	{
-	"( Начать шифрование )", work,
-	"( Открыть файл )",file_chipher,
-	"( О шифре )", about_cipher,
-	 "( Выход )",Exit_prog
+	"( Старт )", work,
+	"( Работа с файлом )",file_chipher,
+	"( Информация о  шифре )", about_cipher,
+	"( Выход )",Exit_prog
 	};
 	main_menu(menu_items, 4);
 	return 0;
